1d597b0f627927a77239b34094cdb1c603f3eafc
mehul455 <gmehul127@gmail.com>
2022-01-30 22:26:51 +0530

    api-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
 function App() {
+ const [text,setText]=useState("");
+ const [body,setBody]=useState("");
+ const [userid,setUser]=useState("");
+const saveData=(e)=>{
+  e.preventDefault()
+  let data={text,body,userid}
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(data)
+  }).then((resp)=>{
+    resp.json().then((result)=>{
+      console.warn("result",result)
+    })
+  })
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{position:"relative",left:"40%",top:"100px"}}>
+    <form onSubmit={saveData}>
+      <h1>Task</h1>
+      <input type="text" name="text" value={text} onChange={(e)=>{setText(e.target.value)}}  required/> <br /> <br />
+      <input type="text" name="body"  value={body} onChange={(e)=>{setBody(e.target.value)}} required/> <br /> <br />
+      <input type="text" name="userid"  value={userid} onChange={(e)=>{setUser(e.target.value)}} required/> <br /> <br />
+      <button  >Save New User</button>
+    </form>
     </div>
   );
 }
-
 export default App;
